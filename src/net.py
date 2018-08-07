e1aa93aca608628c5dc73068a74554b3ea35ba1f
zqp <1316315878@qq.com>
2018-08-07 17:58:09 +0800

    modify net.py

--- src/net.py
+++ src/net.py
@@ -6,14 +6,26 @@ from proto import caffe_pb2
 from layers.data_layer import DataLayer
 from layers.conv_layer import ConvolutionLayer
 class Net:
-    def __init__(self,parameter):
+    def __init__(self,prototxt=None,parameter=None):
+        if prototxt:
+            parameter = caffe_pb2.NetParameter()
+            text_format.Merge(open(prototxt).read(),parameter)
+
+        if not parameter:
+            sys.stderr.write("error init\n")
+
         self.__register_layers = ["Data","Convolution"]
         self.__parameter = parameter
         self.__blobs  = {}
         self.__layers = []
+        self.__layer_names = []
 
+        self.__init_from_parameter()
+        print(self.__layer_names)
+
+    def __init_from_parameter(self):
         if self.__parameter.input:
-            self.__blobs[self.__parameter.input[0]] = np.array((self.__parameter.input_dim),dtype=np.float32)
+            self.__blobs[self.__parameter.input[0]] = None
 
         for layer in self.__parameter.layer:
             if layer.type in self.__register_layers:
@@ -30,11 +42,14 @@ class Net:
                         sys.exit(1)
 
                 self.__layers.append(eval("%sLayer"%layer.type)(layer))
+                self.__layer_names.append(layer.name)
             else:
                 sys.stderr.write("unknown layer %s\n"%layer.type)
                 sys.exit(1)
 
-        print("bolbs: ",self.__blobs.keys())
+    def load_weights(self,weight_file):
+        net_parameter = caffe_pb2.NetParameter()
+        net_parameter.ParseFromString(open(weight_file,"rb").read())
 
     def input(self,input):
         assert isinstance(input,dict),"input is not dict"
@@ -45,19 +60,20 @@ class Net:
                 sys.stderr.write("blob %s is not exist\n"%key)
                 sys.exit(1)
 
-
     def forward_layer(self,index=0):
-        print(self.__blobs)
         for layer in self.__layers[index:]:
             layer.forward(self.__blobs)
 
 if __name__=="__main__":
     input = {}
     input["data"] = np.array([1,2,3],dtype=np.float32)
+    prototxt = "/home/zqp/install_lib/models/mnist/deploy.prototxt"
     prototxt = "/home/zqp/github/pcnn/examples/mnist/deploy.prototxt"
-    net_parameter = caffe_pb2.NetParameter()
-    text_format.Merge(open(prototxt).read(),net_parameter)
+    weight_file = "/home/zqp/install_lib/models/mnist/weight.caffemodel"
 
-    net = Net(net_parameter)
-    net.input(input)
-    net.forward_layer(0)
+    net = Net(prototxt=prototxt)
+    #text_format.Merge(open(prototxt).read(),net_parameter)
+
+
+    #text_format.PrintMessage(net_parameter,open("test1.weights","w"))
+
